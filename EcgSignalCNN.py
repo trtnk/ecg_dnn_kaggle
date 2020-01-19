e81d74aa4bb02f2185640edf6e8a44eeae7c8739
Taro Tanaka <st200register@gmail.com>
2020-01-19 22:39:50 +0900

    "fixed to simple code"

--- EcgSignalCNN.py
+++ EcgSignalCNN.py
@@ -8,16 +8,11 @@ class EcgSignalCNN(nn.Module):
         # convolution layers
         self.conv = nn.Conv1d(1, 32, 5)
 
-        self.conv11 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv12 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv21 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv22 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv31 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv32 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv41 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv42 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv51 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv52 = nn.Conv1d(32, 32, 5, padding=2)
+        self.bottle1 = bottleNeckConv2D(32, 32)
+        self.bottle2 = bottleNeckConv2D(32, 32)
+        self.bottle3 = bottleNeckConv2D(32, 32)
+        self.bottle4 = bottleNeckConv2D(32, 32)
+        self.bottle5 = bottleNeckConv2D(32, 32)
 
         self.pool = nn.MaxPool1d(5, stride=2)
         self.fc1 = nn.Linear(32*2, 32)
@@ -25,22 +20,11 @@ class EcgSignalCNN(nn.Module):
         self.fc3 = nn.Linear(32, 5)
 
     def forward(self, x):
-        x_tmp = self.conv(x)
-        x = F.relu(self.conv11(x_tmp))
-        x = F.relu(self.conv12(x) + x_tmp)
-        x_tmp = self.pool(x)
-        x = F.relu(self.conv21(x_tmp))
-        x = F.relu(self.conv22(x) + x_tmp)
-        x_tmp = self.pool(x)
-        x = F.relu(self.conv31(x_tmp))
-        x = F.relu(self.conv32(x) + x_tmp)
-        x_tmp = self.pool(x)
-        x = F.relu(self.conv41(x_tmp))
-        x = F.relu(self.conv42(x) + x_tmp)
-        x_tmp = self.pool(x)
-        x = F.relu(self.conv51(x_tmp))
-        x = F.relu(self.conv52(x) + x_tmp)
-        x = self.pool(x)
+        x = self.conv(x)
+        x = self.bottle1(x)
+        x = self.bottle2(x)
+        x = self.bottle3(x)
+        x = self.bottle4(x)
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
@@ -97,14 +81,10 @@ class EcgSignalCNN2(nn.Module):
         # convolution layers
         self.conv = nn.Conv1d(1, 32, 5)
 
-        self.conv11 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv12 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv21 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv22 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv31 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv32 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv41 = nn.Conv1d(32, 32, 5, padding=2)
-        self.conv42 = nn.Conv1d(32, 32, 5, padding=2)
+        self.bottle1 = bottleNeckConv2D(32, 32)
+        self.bottle2 = bottleNeckConv2D(32, 32)
+        self.bottle3 = bottleNeckConv2D(32, 32)
+        self.bottle4 = bottleNeckConv2D(32, 32)
 
         self.pool = nn.MaxPool1d(5, stride=2)
         self.fc1 = nn.Linear(32*8, 32)
@@ -112,19 +92,11 @@ class EcgSignalCNN2(nn.Module):
         self.fc3 = nn.Linear(32, 5)
 
     def forward(self, x):
-        x_tmp = self.conv(x)
-        x = F.relu(self.conv11(x_tmp))
-        x = F.relu(self.conv12(x) + x_tmp)
-        x_tmp = self.pool(x)
-        x = F.relu(self.conv21(x_tmp))
-        x = F.relu(self.conv22(x) + x_tmp)
-        x_tmp = self.pool(x)
-        x = F.relu(self.conv31(x_tmp))
-        x = F.relu(self.conv32(x) + x_tmp)
-        x_tmp = self.pool(x)
-        x = F.relu(self.conv41(x_tmp))
-        x = F.relu(self.conv42(x) + x_tmp)
-        x = self.pool(x)
+        x = self.conv(x)
+        x = self.bottle1(x)
+        x = self.bottle2(x)
+        x = self.bottle3(x)
+        x = self.bottle4(x)
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
@@ -137,3 +109,17 @@ class EcgSignalCNN2(nn.Module):
         for s in size:
             num_features *= s
         return num_features
+
+class bottleNeckConv2D(nn.Module):
+    def __init__(self, in_channels, mid_channels):
+        super(bottleNeckConv2D, self).__init__()
+
+        self.conv1 = nn.Conv1d(in_channels, mid_channels, 5, padding=2)
+        self.conv2 = nn.Conv1d(in_channels, mid_channels, 5, padding=2)
+        self.pool = nn.MaxPool1d(5, stride=2)
+
+    def forward(self, x):
+        x = x + self.conv2(F.relu(self.conv1(x)))
+        x = F.relu(x)
+        x = self.pool(x)
+        return x
